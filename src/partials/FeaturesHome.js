f415639e64c86a25d4bc446566d11b0a5f96a6a8
abhasarya <abhas@nextdoor.com>
2020-10-20 00:56:43 -0400

    fix icons

--- src/partials/FeaturesHome.js
+++ src/partials/FeaturesHome.js
@@ -1,5 +1,10 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Transition from '../utils/Transition.js';
+import {
+  FaBolt,
+  FaClipboardList,
+  FaLock
+} from 'react-icons/fa';
 
 function FeaturesHome() {
 
@@ -95,9 +100,7 @@ function FeaturesHome() {
                     <div className="text-gray-600">Through a single interface, patients can keep track of their surveys and medication schedule</div>
                   </div>
                   <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
-                    <svg className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M11.953 4.29a.5.5 0 00-.454-.292H6.14L6.984.62A.5.5 0 006.12.173l-6 7a.5.5 0 00.379.825h5.359l-.844 3.38a.5.5 0 00.864.445l6-7a.5.5 0 00.075-.534z" />
-                    </svg>
+                    <FaBolt/>
                   </div>
                 </a>
                 <a
@@ -110,9 +113,7 @@ function FeaturesHome() {
                     <div className="text-gray-600">Our adaptive survey technology leverages clinically tested PROs, unique to your field, that takes the guesswork out of your patient questionnaires</div>
                   </div>
                   <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
-                    <svg className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z" fillRule="nonzero" />
-                    </svg>
+                    <FaClipboardList/>
                   </div>
                 </a>
                 <a
@@ -125,9 +126,7 @@ function FeaturesHome() {
                     <div className="text-gray-600">We can leverage your existing EHR to allow patients to securely register with their existing accounts in your EMR</div>
                   </div>
                   <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
-                    <svg className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M11.334 8.06a.5.5 0 00-.421-.237 6.023 6.023 0 01-5.905-6c0-.41.042-.82.125-1.221a.5.5 0 00-.614-.586 6 6 0 106.832 8.529.5.5 0 00-.017-.485z" fill="#191919" fillRule="nonzero" />
-                    </svg>
+                    <FaLock/>
                   </div>
                 </a>
               </div>
